31ddad34aad5fddc9bbd2ace3ad5f55b398623a3
yuanjunling <261412489@qq.com>
2018-11-07 16:17:14 +0800

    测试报告集成

--- Yuanjunling1/Hello word/jjjjjj.py
+++ Yuanjunling1/Hello word/jjjjjj.py
@@ -1,8 +1,9 @@
 #coding=utf-8
 import requests
 import unittest
-import HTMLTestRunner,time
-import APIBOSS
+from HTMLTestRunner import HTMLTestRunner
+import APIBOSS,time
+
 
 
 testunit = unittest.TestSuite()
@@ -10,10 +11,10 @@ testunit = unittest.TestSuite()
 testunit.addTest(unittest.makeSuite(APIBOSS.ApiBossTest))
 # #定义个报告存放路径，支持相对路径。
 now = time.strftime("%Y-%m-%M-%H_%M_%S", time.localtime(time.time()))
-wwwa ='D:\\apitest.html'+now+'result.html'
+wwwa ='D:\\apitest'+now+'result.html'
 
 fp = file(wwwa,'wb')
-runner = HTMLTestRunner.HTMLTestRunner(
+runner = HTMLTestRunner(
 
     stream=fp,
     title=u"渠道助手测试报告",
@@ -21,6 +22,6 @@ runner = HTMLTestRunner.HTMLTestRunner(
     verbosity=2
 )
 #执行测试用例
-runner = unittest.TextTestRunner()
+# runner = unittest.TextTestRunner()
 runner.run(testunit)
 fp.close()
