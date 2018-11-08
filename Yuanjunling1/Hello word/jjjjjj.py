b33e9c0a0672222ef5961c763d2ba8a922c219a8
yuanjunling <261412489@qq.com>
2018-11-08 11:46:55 +0800

    API自动化集成优化

--- Yuanjunling1/Hello word/jjjjjj.py
+++ Yuanjunling1/Hello word/jjjjjj.py
@@ -2,13 +2,14 @@
 import requests
 import unittest
 from HTMLTestRunner import HTMLTestRunner
-import APIBOSS,time
+import APIBOSS,time,Qudzs
 
 
 
 testunit = unittest.TestSuite()
 #将测试用例加入到测试套件中
 testunit.addTest(unittest.makeSuite(APIBOSS.ApiBossTest))
+testunit.addTest(unittest.makeSuite(Qudzs.ApiQudzs))
 # #定义个报告存放路径，支持相对路径。
 now = time.strftime("%Y-%m-%M-%H_%M_%S", time.localtime(time.time()))
 wwwa ='D:\\apitest'+now+'result.html'
