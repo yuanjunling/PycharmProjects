db6592d7a473762dbe8fbbd1975bd7c27991d10a
yuanjunling <261412489@qq.com>
2018-11-15 15:08:56 +0800

    ddt数据驱动

--- Yuanjunling1/Hello word/Qudzs.py
+++ Yuanjunling1/Hello word/Qudzs.py
@@ -4,35 +4,13 @@ import re
 import unittest
 import pymysql
 import json
-import random,time,sys,string
-
-class ApiQudzs(unittest.TestCase):
-    def setUp(self):
-        self.log = 'https://login.dev.egtcp.com/cas-server/login?service=https://channel.dev.egtcp.com/cas'#渠道助手端登录网址
-        self.url = 'https://channel.dev.egtcp.com/channel/index?messageCount=31&qrcodeUrl=http%3A%2F%2Ft.cn%2FRr6wq9Y&locale=zh_CN'
-        self.apilog = 'https://account.dev.egtcp.com/api/user/login'
-
-    def testAddSupplier(self):
-
-        url = self.url
-        log_test()#登录函数
-        a1 = s.get(url)
-        #断言验证的账号密码验证码
-        self.assertEqual(test_json['username'],'yuanjlll')
-        self.assertEqual(test_json['password'],'111111')
-        self.assertEqual(test_json['captcha'],'greattao0818')
-        self.assertIn(b"https://channel.dev.egtcp.com",a1.content)
-        self.assertEqual(a1.status_code,200)
-        #验证供应商名称是否重复
-        r = s.get("https://channel.dev.egtcp.com/channel/customer/new")
-        self.assertIn(b"Hamilton Watkins",r.content)
-        Suppliername1 = u'供应商测试数据%d'%random.randrange(1, 9999,)
-        Suppliername = {'customerName':Suppliername1,'role':'2'}
-        add = s.post("https://channel.dev.egtcp.com/channel/customer/checkname",data=Suppliername)
-        #随机生成字符串
-        random1 =''.join(random.sample(string.ascii_letters + string.digits, 50))
-        #供应商数据
-        Supplier = {
+import random,time,sys,string,ddt
+s = requests.Session()#保持Cookie
+#测试数据
+random1 =''.join(random.sample(string.ascii_letters + string.digits, 50))#随机生成字符串
+Suppliername1 = u'供应商测试数据%d'%random.randrange(1, 9999,)
+Supplier = [
+{
             'address':random1,
             'company':Suppliername1,
             'contactEmail':' @qq.com',
@@ -50,14 +28,82 @@ class ApiQudzs(unittest.TestCase):
             'status':'3',
             'test':'20',
             'status':'4',
+        },
+{
+            'address':random1,
+            'company':u'供应商测试数据%d'%random.randrange(1, 99999,),
+            'contactEmail':' @qq.com',
+            'contactMobileNumber':random.choice(['139','188','185','136','158','151'])+"".join(random.choice("0123456789") for i in range(8)),
+            'contactName':random.choice( [u"奥特曼", u"舞法美少女", u"关公",u"张飞", u"奥黛丽赫本",u"朱茵"]),
+            'customerName':u'供应商测试数据%d'%random.randrange(1, 99999,),
+            'customerRequirements':'1',
+            'customerRequirements':'2',
+            'customerRequirements':'8',
+            'district':'330523',
+            'industry':'20',
+            'province':'330000',
+            'role':'2',
+            'shi':'330500',
+            'status':'3',
+            'test':'20',
+            'status':'4',
+        },
+{
+            'address':random1,
+            'company':u'供应商测试数据%d'%random.randrange(1, 99999,),
+            'contactEmail':' @qq.com',
+            'contactMobileNumber':random.choice(['139','188','185','136','158','151'])+"".join(random.choice("0123456789") for i in range(8)),
+            'contactName':random.choice( [u"奥特曼", u"舞法美少女", u"关公",u"张飞", u"奥黛丽赫本",u"朱茵"]),
+            'customerName':u'供应商测试数据%d'%random.randrange(1, 99999,),
+            'customerRequirements':'1',
+            'customerRequirements':'2',
+            'customerRequirements':'8',
+            'district':'330523',
+            'industry':'20',
+            'province':'330000',
+            'role':'2',
+            'shi':'330500',
+            'status':'3',
+            'test':'20',
+            'status':'4',
         }
+
+
+]
+@ddt.ddt
+class ApiQudzs(unittest.TestCase):
+    def setUp(self):
+        self.log = 'https://login.dev.egtcp.com/cas-server/login?service=https://channel.dev.egtcp.com/cas'#渠道助手端登录网址
+        self.url = 'https://channel.dev.egtcp.com/channel/index?messageCount=31&qrcodeUrl=http%3A%2F%2Ft.cn%2FRr6wq9Y&locale=zh_CN'
+        self.apilog = 'https://account.dev.egtcp.com/api/user/login'
+
+    @ddt.data(*Supplier)
+    def testAddSupplier(self,Supplier):
+
+        url = self.url
+        log_test()#登录函数
+        a1 = s.get(url)
+        #断言验证的账号密码验证码
+        self.assertEqual(test_json['username'],'yuanjlll')
+        self.assertEqual(test_json['password'],'111111')
+        self.assertEqual(test_json['captcha'],'greattao0818')
+        self.assertIn(b"https://channel.dev.egtcp.com",a1.content)
+        self.assertEqual(a1.status_code,200)
+
+        r = s.get("https://channel.dev.egtcp.com/channel/customer/new")
+        self.assertIn(b"Hamilton Watkins",r.content)
+        # Suppliername1 = u'供应商测试数据%d'%random.randrange(1, 9999,)
+        # 验证供应商名称是否重复
+        Suppliername = {'customerName':Suppliername1,'role':'2'}
+        add = s.post("https://channel.dev.egtcp.com/channel/customer/checkname",data=Suppliername)
         #添加供应商
         s.post("https://channel.dev.egtcp.com/channel/customer",data=Supplier)
-        self.assertEqual(Supplier['customerName'],Suppliername1)
+
 
 
     def testAddBuyer(self):
         log_test()
+        print s.get("https://channel.dev.egtcp.com/channel/customer/new").text
 
 
 
@@ -65,10 +111,11 @@ class ApiQudzs(unittest.TestCase):
 
 
 def log_test():
-    global s
-    s = requests.Session()  # 保持Cookie
+    # global s
+    # s = requests.Session()
+
     global test_json
-    global a
+
 
     apilog = 'https://account.dev.egtcp.com/api/user/login'
     test_json = {'username': 'yuanjlll',
