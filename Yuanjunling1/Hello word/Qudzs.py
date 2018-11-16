26f2ae464553dcaffa803b0773f0a66bbc72d5f2
yuanjunling <261412489@qq.com>
2018-11-16 15:45:58 +0800

    接口脚本优化

--- Yuanjunling1/Hello word/Qudzs.py
+++ Yuanjunling1/Hello word/Qudzs.py
@@ -4,13 +4,13 @@ import re
 import unittest
 import pymysql
 import json
-import random,time,sys,string,ddt
+import random,time,sys,string
 s = requests.Session()#保持Cookie
 #测试数据
 random1 =''.join(random.sample(string.ascii_letters + string.digits, 50))#随机生成字符串
 Suppliername1 = u'供应商测试数据%d'%random.randrange(1, 9999,)
-Supplier = [
-{
+Supplier = {
+
             'address':random1,
             'company':Suppliername1,
             'contactEmail':' @qq.com',
@@ -28,58 +28,15 @@ Supplier = [
             'status':'3',
             'test':'20',
             'status':'4',
-        },
-{
-            'address':random1,
-            'company':u'供应商测试数据%d'%random.randrange(1, 99999,),
-            'contactEmail':' @qq.com',
-            'contactMobileNumber':random.choice(['139','188','185','136','158','151'])+"".join(random.choice("0123456789") for i in range(8)),
-            'contactName':random.choice( [u"奥特曼", u"舞法美少女", u"关公",u"张飞", u"奥黛丽赫本",u"朱茵"]),
-            'customerName':u'供应商测试数据%d'%random.randrange(1, 99999,),
-            'customerRequirements':'1',
-            'customerRequirements':'2',
-            'customerRequirements':'8',
-            'district':'330523',
-            'industry':'20',
-            'province':'330000',
-            'role':'2',
-            'shi':'330500',
-            'status':'3',
-            'test':'20',
-            'status':'4',
-        },
-{
-            'address':random1,
-            'company':u'供应商测试数据%d'%random.randrange(1, 99999,),
-            'contactEmail':' @qq.com',
-            'contactMobileNumber':random.choice(['139','188','185','136','158','151'])+"".join(random.choice("0123456789") for i in range(8)),
-            'contactName':random.choice( [u"奥特曼", u"舞法美少女", u"关公",u"张飞", u"奥黛丽赫本",u"朱茵"]),
-            'customerName':u'供应商测试数据%d'%random.randrange(1, 99999,),
-            'customerRequirements':'1',
-            'customerRequirements':'2',
-            'customerRequirements':'8',
-            'district':'330523',
-            'industry':'20',
-            'province':'330000',
-            'role':'2',
-            'shi':'330500',
-            'status':'3',
-            'test':'20',
-            'status':'4',
         }
 
-
-]
-@ddt.ddt
 class ApiQudzs(unittest.TestCase):
     def setUp(self):
         self.log = 'https://login.dev.egtcp.com/cas-server/login?service=https://channel.dev.egtcp.com/cas'#渠道助手端登录网址
         self.url = 'https://channel.dev.egtcp.com/channel/index?messageCount=31&qrcodeUrl=http%3A%2F%2Ft.cn%2FRr6wq9Y&locale=zh_CN'
         self.apilog = 'https://account.dev.egtcp.com/api/user/login'
 
-    @ddt.data(*Supplier)
-    def testAddSupplier(self,Supplier):
-
+    def testAddSupplier(self):
         url = self.url
         log_test()#登录函数
         a1 = s.get(url)
@@ -89,7 +46,6 @@ class ApiQudzs(unittest.TestCase):
         self.assertEqual(test_json['captcha'],'greattao0818')
         self.assertIn(b"https://channel.dev.egtcp.com",a1.content)
         self.assertEqual(a1.status_code,200)
-
         r = s.get("https://channel.dev.egtcp.com/channel/customer/new")
         self.assertIn(b"Hamilton Watkins",r.content)
         # Suppliername1 = u'供应商测试数据%d'%random.randrange(1, 9999,)
@@ -98,12 +54,25 @@ class ApiQudzs(unittest.TestCase):
         add = s.post("https://channel.dev.egtcp.com/channel/customer/checkname",data=Suppliername)
         #添加供应商
         s.post("https://channel.dev.egtcp.com/channel/customer",data=Supplier)
+        #进入我的客户列表页面
+        s.get("https://channel.dev.egtcp.com/channel/customer/my/list")
+        #用sql查询客户ID
+        sql = "SELECT id FROM channel_customer WHERE company=%s"
+        resultObj = test_sql(sql,Suppliername1)
+
+        # for x in resultObj:
+        #      print x['id']
+        # print x
+        # print resultObj
+
+        s.get("https://channel.dev.egtcp.com/channel/mycustomer/detail/manager?customerid=%d"%resultObj[0]['id'])
+
 
 
 
     def testAddBuyer(self):
         log_test()
-        print s.get("https://channel.dev.egtcp.com/channel/customer/new").text
+        s.get("https://channel.dev.egtcp.com/channel/customer/new")
 
 
 
