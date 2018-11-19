f75fddacbc8083f806c09ceeea57c0682f33ab5d
yuanjunling <261412489@qq.com>
2018-11-19 17:28:38 +0800

    面向对象 方法继承

--- Yuanjunling1/Hello word/object oriented.py
+++ Yuanjunling1/Hello word/object oriented.py
@@ -1,15 +1,15 @@
 #coding=utf-8
-class Employee:
-    '所以员工的基类'
-    empCount = 0
-    def __init__(self,name,salary):
-        self.name = name
-        self.salary = salary
-        Employee.empCount +=1
-    def displayCount(self):
-        print "Total Employee %d " % Employee.empCount
-    def displayEmployee(self):
-        print "name : ",self.name,", Salary: ",self.salary
+# class Employee:
+#     '所以员工的基类'
+#     empCount = 0
+#     def __init__(self,name,salary):
+#         self.name = name
+#         self.salary = salary
+#         Employee.empCount +=1
+#     def displayCount(self):
+#         print "Total Employee %d " % Employee.empCount
+#     def displayEmployee(self):
+#         print "name : ",self.name,", Salary: ",self.salary
 
 
 
@@ -22,30 +22,30 @@ class Employee:
 #
 # t = Test()
 # t.prt()
-"创建 Employee 类的第一个对象"
-emp1 = Employee("yuan",3000)
-"创建 Employee 类的第二个对象"
-emp2 = Employee("jun",5000)
-emp1.displayEmployee()
-emp2.displayEmployee()
-print "Total Employee %d" % Employee.empCount
-emp1.age = 9
-print hasattr(emp1,'age') # 如果存在 'age' 属性返回 True。
-del emp2.name #删除name属性
-print hasattr(emp2,'name')
-print Employee.__doc__
-print Employee.__name__
-
-class Child(Employee):
-    def __init__(self):
-        print "调用子类构造方法"
+# "创建 Employee 类的第一个对象"
+# emp1 = Employee("yuan",3000)
+# "创建 Employee 类的第二个对象"
+# emp2 = Employee("jun",5000)
+# emp1.displayEmployee()
+# emp2.displayEmployee()
+# print "Total Employee %d" % Employee.empCount
+# emp1.age = 9
+# print hasattr(emp1,'age') # 如果存在 'age' 属性返回 True。
+# del emp2.name #删除name属性
+# print hasattr(emp2,'name')
+# print Employee.__doc__
+# print Employee.__name__
+#
+# class Child(Employee):
+#     def __init__(self):
+#         print "调用子类构造方法"
 
 
 
 
 
 class Parent:  # 定义父类
-    parentAttr = 100
+    parentAttr = 0
 
     def __init__(self):
         print "调用父类构造函数"
@@ -66,3 +66,10 @@ class Child(Parent):  # 定义子类
 
     def childMethod(self):
         print '调用子类方法'
+
+
+c = Child()          # 实例化子类
+c.childMethod()      # 调用子类的方法
+c.parentMethod()     # 调用父类方法
+c.setAttr(300)       # 再次调用父类的方法 - 设置属性值
+c.getAttr()          # 再次调用父类的方法 - 获取属性值
