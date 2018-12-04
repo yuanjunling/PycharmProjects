e5f4c50194927e8c6e1a9db027166b1bb95b6432
yuanjunling <261412489@qq.com>
2018-12-04 10:24:36 +0800

    测试报告集成

--- Yuanjunling1/Hello word/Exceltest.py
+++ Yuanjunling1/Hello word/Exceltest.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from xlrd import xldate_as_tuple
 #路径前加 r，读取的文件路径
 file_path = r'D:\test111.xls'
+file_path1 ='D://test111.xls'
 #文件路径的中文转码
 file_path = file_path.decode('utf-8')
 #获取数据
@@ -22,21 +23,29 @@ col_values = table.col_values(1)
 cell_value = table.cell(0,0).value
 print cell_value
 
-def fangfa(a=None,b=None):
-    # 路径前加 r，读取的文件路径
-    file_path = r'D:\test111.xls'
-    # 文件路径的中文转码
-    file_path = file_path.decode('utf-8')
-    # 获取数据
-    data = xlrd.open_workbook(file_path)
-    # 获取sheet
-    table = data.sheet_by_name('Sheet1')
-    # 获取总行数
-    nrows = table.nrows
-    # 获取总列数
-    ncols = table.ncols
-    # 获取一个单元格的数值，例如第1行第2列
-    cell_value = table.cell(a, b).value
+def reacell_value(file_path,a,b):
+    file_path = file_path.decode('utf-8')    # 文件路径的中文转码
+    data = xlrd.open_workbook(file_path)# 获取数据
+    cell_value = table.cell(a,b).value # 获取一个单元格的数值，例如第1行第2列
     return cell_value
-fangfa = fangfa(1,1)
+
+fangfa = reacell_value(file_path,1,1)
 print fangfa
+
+# def readExcelDataByName(fileName, sheetName):
+#     table = None
+#     errorMsg = None
+#     try:
+#         data = xlrd.open_workbook(fileName)
+#         table = data.sheet_by_index(sheetName)
+#     except Exception, msg:
+#         errorMsg = msg
+#     return table, errorMsg
+
+def rea_cell_value(file_path,a):
+    file_path = file_path.decode('utf-8')    # 文件路径的中文转码
+    data = xlrd.open_workbook(file_path)# 获取数据
+    col_values = table.row_values(a) # 获取一个单元格的数值，例如第1行第2列
+    return col_values
+fangfa1 = rea_cell_value(file_path,1)
+print str(fangfa1).decode("unicode_escape").encode("utf-8")
