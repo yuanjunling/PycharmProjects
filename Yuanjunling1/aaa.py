de7c494896c4f521654a6368ae8783fa696966ea
Yuan Jun Ling <261412489@qq.com>
2019-07-13 09:54:37 +0800

    接口学习

--- Yuanjunling1/aaa.py
+++ Yuanjunling1/aaa.py
@@ -1,14 +1,38 @@
 #coding=utf-8
-from selenium import webdriver
-driver = webdriver.Firefox()
-driver.get("http://www.baidu.com/")
-#点击登录链接
-driver.find_element_by_name("tj_login").click()
-#通过二次定位找到用户名输入框
-div=driver.find_element_by_class_name("tang-content").find_element_by_name("userName")
-div.send_keys("username")
-#输入登录密码
-driver.find_element_by_name("password").send_keys("password")
-#点击登录
-driver.find_element_by_id("TANGRAM__PSP_10__submit").click()
-driver.quit()
+import requests
+import re
+import unittest
+import pymysql
+import json
+import random,time,sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+s = requests.Session()#保持Cookie
+url = 'http://192.168.200.7:30501/api/User/Login'
+headers = {
+    "Content-Type":"application/json; charset=utf-8"
+}
+test_json={"UserName": "13613613602","PassWord": "111111","Endpoint": "1"}
+json_Deom=s.post('http://192.168.200.7:30501/api/User/Login',headers=headers,json=test_json)
+key=json_Deom.json()
+print key["Data"]
+headers1 = {
+    "Content-Type":"application/json; charset=utf-8",
+    "Authorization":key["Data"]
+}
+test_json1={
+	"Page":1,
+	"Num":1
+}
+url1="http://192.168.200.7:30508/patrol/PatrolInspection/GetInspectionList"
+# print s.post(url1,headers=headers1,json=test_json1).json()
+jj= requests.post(url1,headers=headers1,json=test_json1).json()
+# jjj = json.dumps(jj,ensure_ascii=False)
+# print jj["Msg"]
+#接口返回值断言
+
+if jj["Msg"] == u"获取成功":
+    print u"断言成功"
+else:
+    print u"断言失败"
+assert jj["Msg"] == "获取成功"
